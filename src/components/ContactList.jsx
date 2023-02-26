5f8a8240d28be1995e8e43647c0dcf74fb899c0c
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-02-27 01:22:25 +0300

    feat: added the contact list component

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import {FcPhone} from "react-icons/fc"
+import { editContact, editId, modalOpen, removeContact } from '../redux/contactSlice'
 
 const ContactList = () => {
+
+    const dispatch = useDispatch();
+    const { isOpen, contacts, filter } = useSelector((state) => state.contact);
+
+  const getFilteredContacts = (contacts, filter) => {
+    return contacts.filter((contact) =>
+      contact?.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+  const filteredContacts = getFilteredContacts(contacts, filter);
+  
   return (
-    <div>ContactList</div>
-  )
-}
+    <ul>
+      {filteredContacts.map((contact) => (
+        <li key={contact.id} id={contact.id}>
+          <div>
+            <FcPhone />
+            <p>{contact.name + ": " + contact.number}</p>
+          </div>
+          <button
+            type="button"
+            onClick={() => dispatch(removeContact(contact.id))}
+          >
+            Sil
+          </button>
+          <button
+           
+            onClick={() => {
+              dispatch(editId(contact.id))
+              return dispatch(modalOpen());
+            }}
+          >
+            Düzenle
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default ContactList
