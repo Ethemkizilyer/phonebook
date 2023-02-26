6a346075a450a22b45b7e807c3b4d87e289fde2c
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-02-26 22:48:46 +0300

    feat: redux-persist added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
+import { useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  const asd = useSelector(state=>state.contact)
+
   return (
     <div className="App">
-      
+      <h1>Phonebook</h1>
     </div>
   );
 }
---
+++ src/redux/store.jsx
@@ -0,0 +1,38 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import contactSlice from "./contactSlice";
+
+const persistContactConfig={
+    key:"contactbook-items",
+    version:1,
+    storage,
+    whitelist:["contacts"],
+}
+
+const persistedContactsReducer = persistReducer(
+  persistContactConfig,
+  contactSlice
+);
+
+export const store = configureStore({
+    reducer:{
+        contact:persistedContactsReducer,
+    },
+    middleware:getDefaultMiddleware=>getDefaultMiddleware({
+        serializableCheck:{
+       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        }
+    }),
+      devTools: process.env.NODE_ENV !== "production",
+})
+export const persistor = persistStore(store);
